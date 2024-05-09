8b1b8aba32cf8b8564a3a78d6d47f2cfd24d487e
kprathima <prathima.kondragunta@gmail.com>
2024-05-09 13:06:54 -0400

    ADD files

--- test.java
+++ test.java
@@ -1,1 +1,7 @@
-sasfga kdskjd jhdskj
+public class test {
+    public static void main(String[] args) {
+        String txt = "Hello World";
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.toLowerCase());
+    }
+}
