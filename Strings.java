4884484448e5601c50f706f15afeb234ce3f38cf
kprathima <prathima.kondragunta@gmail.com>
2024-05-09 16:34:18 -0400

    abc

---
+++ Strings.java
@@ -0,0 +1,9 @@
+public class Strings {
+    public static void main(String args[]) {
+        String firstName = "John";
+        String middleName = "bose";
+        String lastName = "Doe";
+
+        System.out.println(firstName + " " + middleName +"  "+ lastName);
+    }
+}
