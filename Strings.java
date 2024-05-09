46757dab3a0a4fe36f4a0874523253029ecbb119
kprathima <prathima.kondragunta@gmail.com>
2024-05-09 16:42:13 -0400

    change

--- Strings.java
+++ Strings.java
@@ -2,8 +2,8 @@ public class Strings {
     public static void main(String args[]) {
         String firstName = "John";
         String middleName = "bose";
-        String lastName = "Doe";
+        String lastName = "sss";
 
-        System.out.println(firstName + " " + middleName +"  "+ lastName);
+        System.out.println(firstName + " " + middleName + "  " + lastName);
     }
 }
