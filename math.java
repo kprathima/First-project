3bbcb2dc39174a76f8b872bc92e12e60c4d5620d
kprathima <prathima.kondragunta@gmail.com>
2024-05-10 12:08:18 -0400

    math file

---
+++ math.java
@@ -0,0 +1,14 @@
+public class math {
+    public static void main(String[] args) {
+        int x = 18;
+        int y = 18;
+        if (x > y) {
+            System.out.println("x is greater than y");
+        } else {
+            System.out.println("x is not grerater than y");}
+            else {
+            
+                System.out.println("x is equal to y");
+            }
+            }
+}}
