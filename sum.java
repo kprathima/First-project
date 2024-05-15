e3483e94477e57adf0d4e858843dc6a8bc12cf27
kprathima <prathima.kondragunta@gmail.com>
2024-05-15 14:24:26 -0400

    changes

--- sum.java
+++ sum.java
@@ -4,8 +4,10 @@ public class sum {
         int num1 = 10 + 20;
         int num2 = num1 + 100;
         int num3 = num1 + num2 + 100;
+        int num4 = num3 + num1;
         System.out.println(num1);
         System.out.println(num2);
         System.out.println(num3);
+        System.out.println(num4);
     }
 }
