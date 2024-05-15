9ce3c4ec0b6132fb6c5b4809e0d25837de77df34
kprathima <prathima.kondragunta@gmail.com>
2024-05-15 13:14:06 -0400

    sum

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class sum {
+
+    public static void main(String[] args) {
+        int num1 = 10 + 20;
+        int num2 = num1 + 100;
+        int num3 = num1 + num2 + 100;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}
