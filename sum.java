9c1fc84e1882561077678f660b830619d1685756
kprathima <prathima.kondragunta@gmail.com>
2024-05-16 12:58:29 -0400

    delete some lines

--- sum.java
+++ sum.java
@@ -3,11 +3,10 @@ public class sum {
     public static void main(String[] args) {
         int num1 = 10 + 20;
         int num2 = num1 + 100;
-        int num3 = num1 + num2 + 100;
-        int num4 = num3 + num1;
+
         System.out.println(num1);
         System.out.println(num2);
         System.out.println(num3);
-        System.out.println(num4);
+
     }
 }
