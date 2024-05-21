76f61a29cfd3970e6543fd56c1bbe352265857d3
kprathima <prathima.kondragunta@gmail.com>
2024-05-21 12:03:43 -0400

    changes

--- sum.java
+++ sum.java
@@ -3,6 +3,7 @@ public class sum {
     public static void main(String[] args) {
         int num1 = 10 + 20;
         int num2 = num1 + 100;
+git status
 
         System.out.println(num1);
         System.out.println(num2);
