e95beaae67ec6b593d9d8c1e7eed367645963d2e
kprathima <prathima.kondragunta@gmail.com>
2024-05-28 14:16:31 -0400

    changes

--- math.java
+++ math.java
@@ -4,10 +4,15 @@ public class math {
         int y = 18;
         if (x > y) {
             System.out.println("x is greater than y");
-        } else {
-            System.out.println("x is not grerater than y");}
+        else 
+        {
+            System.out.println("x is not grerater than y");
+        }
             
             
             
-            }
-            }}}
+            
+            
+        }
+    }
+}
