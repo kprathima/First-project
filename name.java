1cfa467afb1dc8bd2fa267ca1a6e8956cc451c70
kprathima <prathima.kondragunta@gmail.com>
2024-06-04 13:13:27 -0400

    party

---
+++ name.java
@@ -0,0 +1,14 @@
+public class name {
+
+    public static void main(String[] args) {
+        int AA=10;
+        if (AA<10){
+            System.out.println("It's party Time");
+            else 
+
+             System.out.println("No Party");
+            
+            
+        }
+    }
+}
