4bbdae9eb56c46e571c54487338c2e6eb9e07f05
kprathima <prathima.kondragunta@gmail.com>
2024-06-11 09:54:45 -0400

    add change

--- name.java
+++ name.java
@@ -2,10 +2,12 @@ public class name {
 
     public static void main(String[] args) {
         int AA = 10;
-        if (AA < 10) {
-            System.out.println("It's party Time");
-            System.out.println("No Party");
+        if (AA < 10)
+            if (AA > 10) {
+                System.out.println("It's party Time");
+                System.out.println("No Party");
+                System.out.println("it's a party");
 
-        }
+            }
     }
 }
